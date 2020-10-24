13d2485f04e0edf8f9374d7e4f35b8c36bee2e14
Vishal Garg <vishugarg10004@gmail.com>
2020-10-25 01:18:12 +0530

    [Vishal] added methods to peek, pop and remove all elements from stack.

--- StacksAndQueueMSTest/UnitTest1.cs
+++ StacksAndQueueMSTest/UnitTest1.cs
@@ -1,13 +1,38 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StacksAndQueues;
 
 namespace StacksAndQueueMSTest
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        Stacks stacks = new Stacks();
+        /// <summary>
+        /// creating stacks set up, which will run for every test case
+        /// adding values in stacks
+        /// </summary>
+        [TestInitialize]
+        public void StackSetUp()
         {
+            //Arrange
+            stacks.Push(56);
+            stacks.Push(53);
+            stacks.Push(45);
+        }
+        /// <summary>
+        /// Testing peek by giving expected value and taking actual value from method
+        /// </summary>
+        [TestMethod]
+        public void TestingPeekinStacks()
+        {
+            //Add
+            int actual= stacks.Peek();
+            int expected = 45;
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+            
+
         }
     }
 }
