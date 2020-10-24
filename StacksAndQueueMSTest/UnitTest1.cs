b7c018dca011b804bbb8713a833f5e2f6b34201d
Vishal Garg <vishugarg10004@gmail.com>
2020-10-25 00:31:54 +0530

    [Vishal] Welcome Message

---
+++ StacksAndQueueMSTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StacksAndQueueMSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
